200442950783cfb64ba9e925c8564300875a2218
nachille <achille.nicola@gmail.com>
2019-11-09 02:39:30 +0100

    sample api

---
+++ beevel.api/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
+//https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?ranMID=24542&ranEAID=je6NUbpObpQ&ranSiteID=je6NUbpObpQ-8zr7hcFszdUWHRkV1s3r4g&epi=je6NUbpObpQ-8zr7hcFszdUWHRkV1s3r4g&irgwc=1&OCID=AID2000142_aff_7593_1243925&tduid=(ir__kjinnc02pokftmaukk0sohz3x22xgr0fkxy0dgpd00)(7593)(1243925)(je6NUbpObpQ-8zr7hcFszdUWHRkV1s3r4g)()&irclickid=_kjinnc02pokftmaukk0sohz3x22xgr0fkxy0dgpd00&view=aspnetcore-3.0&tabs=visual-studio-code
